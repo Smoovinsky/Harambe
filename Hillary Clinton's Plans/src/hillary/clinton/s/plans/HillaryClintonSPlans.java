5f5ced5bf29214631c23621a739465425eb94981
2161724 <2161724@C2-LSO-M07.IPLeiria.pt>
2016-10-19 13:27:22 +0100

    You will get shot

---
+++ Hillary Clinton's Plans/src/hillary/clinton/s/plans/HillaryClintonSPlans.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hillary.clinton.s.plans;
+
+/**
+ *
+ * @author 2161724
+ */
+public class HillaryClintonSPlans {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        //I possess the plans that can destroy Hillary's Presidencial career
+
+    }
+    
+}
